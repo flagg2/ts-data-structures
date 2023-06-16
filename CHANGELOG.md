6c24e2af6868c2f83ac2aac8348c44a49bad3e46
flagg2 <samo.wittlinger@gmail.com>
2023-06-16 14:15:26 +0200

    feat: update

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# @flagg2/data-structures
+
+## 0.0.4
+
+### Patch Changes
+
+-  export more functions
+
+## 0.0.2
+
+### Patch Changes
+
+-  initial release
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { StructuralMap } from "./StructuralMap"
 import { StructuralSet } from "./StructuralSet"
+import { stringifyObjectWithOrderedKeys, isPlainObject } from "./helpers"
 
-export { StructuralMap, StructuralSet }
+export {
+   StructuralMap,
+   StructuralSet,
+   stringifyObjectWithOrderedKeys,
+   isPlainObject,
+}
